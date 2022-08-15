963b4ea985b34854d8871906b6f7f0755423baf6
youngminshin <cntmin81@gmail.com>
2022-08-15 20:24:48 +0900

    initial commit

---
+++ src/test/java/cntmin81/github/io/springbootrestapi/SpringbootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cntmin81.github.io.springbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
