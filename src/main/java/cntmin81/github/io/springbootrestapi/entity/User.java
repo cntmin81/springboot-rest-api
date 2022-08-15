2fc808e1f57fb891f4f448b550dfb8fb3efd01b7
youngminshin <cntmin81@gmail.com>
2022-08-15 21:02:20 +0900

    유저 엔티티추가

---
+++ src/main/java/cntmin81/github/io/springbootrestapi/entity/User.java
@@ -0,0 +1,31 @@
+package cntmin81.github.io.springbootrestapi.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = true, unique = true, length = 30)
+    private String email;
+
+    @Column(nullable = true, length = 100)
+    private String name;
+}
