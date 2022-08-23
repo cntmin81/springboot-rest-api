ccd0657d73c9facc57ef2d554d590432a5e8f933
Youngmin.Shin <cntmin81@gmail.com>
2022-08-23 14:32:56 +0900

    Enable JpaAuditing

--- src/main/java/cntmin81/github/io/springbootrestapi/SpringbootRestApiApplication.java
+++ src/main/java/cntmin81/github/io/springbootrestapi/SpringbootRestApiApplication.java
@@ -2,7 +2,9 @@ package cntmin81.github.io.springbootrestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringbootRestApiApplication {
 
--- src/main/java/cntmin81/github/io/springbootrestapi/entity/User.java
+++ src/main/java/cntmin81/github/io/springbootrestapi/entity/User.java
@@ -18,7 +18,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
